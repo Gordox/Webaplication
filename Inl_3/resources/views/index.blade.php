44eebb3839f21307aa10151c83249802c00ed2da
Anton_Gordox <anton@gordox.se>
2017-06-25 11:56:40 +0200

    fixes

--- Inl_3/resources/views/index.blade.php
+++ Inl_3/resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <!--Title-->
     <h1>Welcome to Mobile Market</h1>
     <p>We always got something for YOU!</p>
-    <img src="https://vignette3.wikia.nocookie.net/pokemon/images/9/9f/143Snorlax_OS_anime.png/revision/latest?cb=20140924022259">
+    <img src="https://www.metrogroup.de/~/assets/mag/images/logos/jpgs/media-markt-logo.jpg?bc=White&amp;h=310&amp;la=en&amp;w=620">
   </div>
 </div>
 @endsection
---
+++ Inl_4/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@section('content')
+<div>
+  <div style="text-align:center;">
+    <img src="https://se.storebadge.com/uploads/46c8905f1a270eb6323806110040e21d.jpg">
+    <!--Title-->
+    <h1>Welcome to Mobile Market</h1>
+    <p>We always got something for YOU!</p>
+
+  </div>
+</div>
+@endsection
